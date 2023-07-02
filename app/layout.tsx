13482207ce585ce7da3df2ce506271a7cb83ce66
Ashley Redman <ash.redman@outlook.com>
2023-07-02 10:45:22 +0100

    wip

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
-import './globals.css';
+import { Analytics } from '@vercel/analytics/react';
 import { Inter } from 'next/font/google';
+import './globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -12,6 +13,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     return (
         <html lang='en'>
             <body className={inter.className}>{children}</body>
+            <Analytics />
         </html>
     );
 }
